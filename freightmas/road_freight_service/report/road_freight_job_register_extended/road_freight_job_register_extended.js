da1990f6edd8a44e5fc356c78bfd1c9a56b9781e
Lovemore Chigwanda <lchigwanda@outlook.com>
2025-07-07 08:11:11 +0200

    Improvements in Road Freight Reports

---
+++ freightmas/road_freight_service/report/road_freight_job_register_extended/road_freight_job_register_extended.js
@@ -0,0 +1,119 @@
+// Copyright (c) 2025, Zvomaita Technologies (Pvt) Ltd and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Road Freight Job Register Extended"] = {
+    filters: [
+        {
+            fieldname: "date_range",
+            label: __("Date Range"),
+            fieldtype: "Select",
+            options: [
+                "",
+                "Today",
+                "Yesterday",
+                "This Week",
+                "Last Week",
+                "This Month",
+                "Last Month",
+                "This Year",
+                "Last Year",
+                "Custom"
+            ],
+            default: "This Month",
+            on_change: function () {
+                let date_range = frappe.query_report.get_filter_value('date_range');
+                let today = frappe.datetime.get_today();
+                let from_date, to_date;
+
+                switch (date_range) {
+                    case "Today":
+                        from_date = to_date = today;
+                        break;
+                    case "Yesterday":
+                        from_date = to_date = frappe.datetime.add_days(today, -1);
+                        break;
+                    case "This Week":
+                        from_date = frappe.datetime.week_start();
+                        to_date = frappe.datetime.week_end();
+                        break;
+                    case "Last Week":
+                        from_date = frappe.datetime.add_days(frappe.datetime.week_start(), -7);
+                        to_date = frappe.datetime.add_days(frappe.datetime.week_end(), -7);
+                        break;
+                    case "This Month":
+                        from_date = frappe.datetime.month_start();
+                        to_date = frappe.datetime.month_end();
+                        break;
+                    case "Last Month":
+                        from_date = frappe.datetime.add_months(frappe.datetime.month_start(), -1);
+                        to_date = frappe.datetime.add_days(frappe.datetime.month_start(), -1);
+                        break;
+                    case "This Year":
+                        from_date = frappe.datetime.year_start();
+                        to_date = frappe.datetime.year_end();
+                        break;
+                    case "Last Year":
+                        const lastYear = (new Date()).getFullYear() - 1;
+                        from_date = `${lastYear}-01-01`;
+                        to_date = `${lastYear}-12-31`;
+                        break;
+                    default:
+                        from_date = frappe.query_report.get_filter_value('from_date');
+                        to_date = frappe.query_report.get_filter_value('to_date');
+                }
+
+                if (date_range && date_range !== "Custom" && date_range !== "") {
+                    frappe.query_report.set_filter_value('from_date', from_date);
+                    frappe.query_report.set_filter_value('to_date', to_date);
+                }
+            }
+        },
+        {
+            fieldname: "from_date",
+            label: "From Date",
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+        },
+        {
+            fieldname: "to_date",
+            label: "To Date",
+            fieldtype: "Date",
+            default: frappe.datetime.get_today()
+        },
+        {
+            fieldname: "customer",
+            label: "Customer",
+            fieldtype: "Link",
+            options: "Customer"
+        },
+        {
+            fieldname: "status",
+            label: "Status",
+            fieldtype: "Select",
+            options: "\nDraft\nIn Progress\nCompleted\nCancelled"
+        }
+    ],
+
+    onload: function(report) {
+        report.page.add_inner_button('Export to Excel', function() {
+            const filters = report.get_filter_values(true);
+            const query = encodeURIComponent(JSON.stringify(filters));
+            const url = `/api/method/freightmas.api.export_report_to_excel?report_name=Road Freight Job Register Extended&filters=${query}`;
+            window.open(url);
+        }, 'Export');
+
+        report.page.add_inner_button('Export to PDF', function() {
+            const filters = report.get_filter_values(true);
+            const query = encodeURIComponent(JSON.stringify(filters));
+            const url = `/api/method/freightmas.api.export_report_to_pdf?report_name=Road Freight Job Register Extended&filters=${query}`;
+            window.open(url);
+        }, 'Export');
+
+        report.page.add_inner_button('Clear Filters', function() {
+            report.filters.forEach(filter => {
+                let default_value = filter.df.default || "";
+                report.set_filter_value(filter.df.fieldname, default_value);
+            });
+        });
+    }
+};
---
+++ freightmas/road_freight_service/report/road_freight_job_register_extended/road_freight_job_register_extended.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2025, Zvomaita Technologies (Pvt) Ltd and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+def format_date(date_str):
+    if not date_str:
+        return ""
+    try:
+        return frappe.utils.formatdate(date_str, "dd-MMM-yy")
+    except Exception:
+        return date_str
+
+def get_columns():
+    return [
+        {"label": "Job ID", "fieldname": "name", "fieldtype": "Link", "options": "Road Freight Job", "width": 140},
+        {"label": "Job Date", "fieldname": "date_created", "fieldtype": "Data", "width": 90},
+        {"label": "Customer", "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 200},
+        {"label": "Reference", "fieldname": "customer_reference", "fieldtype": "Data", "width": 120},
+        {"label": "Direction", "fieldname": "direction", "fieldtype": "Data", "width": 90},
+        {"label": "Type", "fieldname": "shipment_type", "fieldtype": "Data", "width": 110},
+        {"label": "Cargo Desc.", "fieldname": "cargo_description", "fieldtype": "Data", "width": 180, "align": "left"},
+        {"label": "Load At", "fieldname": "port_of_loading", "fieldtype": "Link", "options": "Port", "width": 100},
+        {"label": "Offload At", "fieldname": "port_of_discharge", "fieldtype": "Link", "options": "Port", "width": 100},
+        {"label": "Drop At", "fieldname": "empty_drop_off_at", "fieldtype": "Link", "options": "Port", "width": 100},
+        {"label": "Load By", "fieldname": "load_by_date", "fieldtype": "Data", "width": 100},
+        {"label": "Offload By", "fieldname": "offload_by_date", "fieldtype": "Data", "width": 100},
+        {"label": "Drop By", "fieldname": "return_empty_by_date", "fieldtype": "Data", "width": 100},
+        {"label": "No Req", "fieldname": "no_of_trucks_required", "fieldtype": "Int", "width": 90},
+        {"label": "No Conf", "fieldname": "trucks_confirmed", "fieldtype": "Int", "width": 90},
+        {"label": "Client Rate", "fieldname": "client_rate", "fieldtype": "Currency", "width": 100},
+        {"label": "Transp Rate", "fieldname": "transporter_rate", "fieldtype": "Currency", "width": 100},
+        {"label": "Pmt Terms", "fieldname": "payment_terms", "fieldtype": "Data", "width": 120},
+        {"label": "Est. Rev", "fieldname": "total_estimated_revenue_base", "fieldtype": "Currency", "width": 110},
+        {"label": "Est. Cost", "fieldname": "total_estimated_cost_base", "fieldtype": "Currency", "width": 110},
+        {"label": "Est. Prft", "fieldname": "total_estimated_profit_base", "fieldtype": "Currency", "width": 110},
+        {"label": "Status", "fieldname": "status", "fieldtype": "Data", "width": 110},
+        {"label": "Completed", "fieldname": "completed_on", "fieldtype": "Data", "width": 100},
+    ]
+
+def execute(filters=None):
+    columns = get_columns()
+    data = []
+
+    filters = filters or {}
+    job_filters = {}
+
+    if filters.get("from_date") and filters.get("to_date"):
+        job_filters["date_created"] = ["between", [filters["from_date"], filters["to_date"]]]
+    if filters.get("customer"):
+        job_filters["customer"] = filters["customer"]
+    if filters.get("status"):
+        job_filters["status"] = filters["status"]
+
+    jobs = frappe.get_all(
+        "Road Freight Job",
+        filters=job_filters,
+        fields=[
+            "name", "date_created", "customer", "customer_reference", "direction", "shipment_type",
+            "cargo_description", "port_of_loading", "port_of_discharge", "empty_drop_off_at",
+            "load_by_date", "offload_by_date", "return_empty_by_date",
+            "no_of_trucks_required", "trucks_confirmed",
+            "client_rate", "transporter_rate", "payment_terms",
+            "total_estimated_revenue_base", "total_estimated_cost_base", "total_estimated_profit_base",
+            "status", "completed_on"
+        ]
+    )
+
+    for job in jobs:
+        data.append({
+            "name": job.get("name", ""),
+            "date_created": format_date(job["date_created"]),
+            "customer": job.get("customer", ""),
+            "customer_reference": job.get("customer_reference", ""),
+            "direction": job.get("direction", ""),
+            "shipment_type": job.get("shipment_type", ""),
+            "cargo_description": job.get("cargo_description", ""),
+            "port_of_loading": job.get("port_of_loading", ""),
+            "port_of_discharge": job.get("port_of_discharge", ""),
+            "empty_drop_off_at": job.get("empty_drop_off_at", ""),
+            "load_by_date": format_date(job.get("load_by_date", "")),
+            "offload_by_date": format_date(job.get("offload_by_date", "")),
+            "return_empty_by_date": format_date(job.get("return_empty_by_date", "")),
+            "no_of_trucks_required": job.get("no_of_trucks_required", 0),
+            "trucks_confirmed": job.get("trucks_confirmed", 0),
+            "client_rate": job.get("client_rate", 0),
+            "transporter_rate": job.get("transporter_rate", 0),
+            "payment_terms": job.get("payment_terms", ""),
+            "total_estimated_revenue_base": job.get("total_estimated_revenue_base", 0),
+            "total_estimated_cost_base": job.get("total_estimated_cost_base", 0),
+            "total_estimated_profit_base": job.get("total_estimated_profit_base", 0),
+            "status": job.get("status", ""),
+            "completed_on": format_date(job.get("completed_on", "")),
+        })
+
+    return columns, data
