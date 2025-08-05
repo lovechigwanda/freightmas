ff3a912ed7098302a3f1348834c6b271e4808049
Lovemore Chigwanda <lchigwanda@outlook.com>
2025-08-05 21:50:41 +0200

    Added Cashbook Report to Freightmas This commit introduces a new Cashbook Report to the Freightmas application, enhancing financial tracking capabilities. The report includes a detailed view of cash transactions, allowing users to filter by date and view cash

---
+++ freightmas/freightmas/report/cashbook_report/cashbook_report.js
@@ -0,0 +1,122 @@
+// Copyright (c) 2025, Zvomaita Technologies (Pvt) Ltd and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Cashbook Report"] = {
+    "filters": [
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+        },
+        {
+            fieldname: "date_range",
+            label: __("Date Range"),
+            fieldtype: "Select",
+            options: [
+                "",
+                "Today",
+                "Yesterday",
+                "This Week",
+                "Last Week",
+                "This Month",
+                "Last Month",
+                "This Year",
+                "Last Year",
+                "Custom"
+            ],
+            default: "This Month",
+            on_change: function() {
+                let date_range = frappe.query_report.get_filter_value('date_range');
+                let today = frappe.datetime.get_today();
+                let from_date, to_date;
+
+                switch (date_range) {
+                    case "Today":
+                        from_date = to_date = today;
+                        break;
+                    case "Yesterday":
+                        from_date = to_date = frappe.datetime.add_days(today, -1);
+                        break;
+                    case "This Week":
+                        from_date = frappe.datetime.week_start();
+                        to_date = frappe.datetime.week_end();
+                        break;
+                    case "Last Week":
+                        from_date = frappe.datetime.add_days(frappe.datetime.week_start(), -7);
+                        to_date = frappe.datetime.add_days(frappe.datetime.week_end(), -7);
+                        break;
+                    case "This Month":
+                        from_date = frappe.datetime.month_start();
+                        to_date = frappe.datetime.month_end();
+                        break;
+                    case "Last Month":
+                        from_date = frappe.datetime.add_months(frappe.datetime.month_start(), -1);
+                        to_date = frappe.datetime.add_days(frappe.datetime.month_start(), -1);
+                        break;
+                    case "This Year":
+                        from_date = frappe.datetime.year_start();
+                        to_date = frappe.datetime.year_end();
+                        break;
+                    case "Last Year":
+                        let today_parts = today.split('-');
+                        let last_year = parseInt(today_parts[0], 10) - 1;
+                        from_date = `${last_year}-01-01`;
+                        to_date = `${last_year}-12-31`;
+                        break;
+                    default:
+                        from_date = frappe.query_report.get_filter_value('from_date');
+                        to_date = frappe.query_report.get_filter_value('to_date');
+                }
+
+                if (date_range && date_range !== "Custom" && date_range !== "") {
+                    frappe.query_report.set_filter_value('from_date', from_date);
+                    frappe.query_report.set_filter_value('to_date', to_date);
+                }
+            }
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_start(),
+            "reqd": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_end(),
+            "reqd": 1
+        },
+        {
+            "fieldname": "account",
+            "label": __("Account"),
+            "fieldtype": "Link",
+            "options": "Account",
+            "get_query": function() {
+                return {
+                    "filters": {
+                        "account_type": ["in", ["Bank", "Cash"]]
+                    }
+                }
+            }
+        }
+    ],
+    
+    onload: function(report) {
+        report.page.add_inner_button(__('Clear Filters'), function() {
+            // Reset filters to default values
+            report.set_filter_value('company', frappe.defaults.get_user_default("Company"));
+            report.set_filter_value('date_range', 'This Month');
+            report.set_filter_value('from_date', frappe.datetime.month_start());
+            report.set_filter_value('to_date', frappe.datetime.month_end());
+            report.set_filter_value('account', '');
+            
+            // Refresh the report
+            report.refresh();
+        });
+    }
+};
---
+++ freightmas/freightmas/report/cashbook_report/cashbook_report.py
@@ -0,0 +1,154 @@
+# Copyright (c) 2025, Zvomaita Technologies (Pvt) Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import formatdate
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    return [
+        {
+            "fieldname": "posting_date",
+            "label": _("Date"),
+            "fieldtype": "Data",
+            "width": 100
+        },
+        {
+            "fieldname": "voucher_no",
+            "label": _("Voucher No"),
+            "fieldtype": "Dynamic Link",  # Changed from Link to Dynamic Link
+            "options": "voucher_type",    # Will use the voucher_type field to determine document type
+            "width": 185,
+            "align": "left"
+        },
+        {
+            "fieldname": "remarks",
+            "label": _("Remarks"),
+            "fieldtype": "Text",
+            "width": 530  # Increased width since we removed contra account
+        },
+        {
+            "fieldname": "debit",
+            "label": _("Debit"),
+            "fieldtype": "Currency",
+            "width": 130
+        },
+        {
+            "fieldname": "credit",
+            "label": _("Credit"),
+            "fieldtype": "Currency",
+            "width": 130
+        },
+        {
+            "fieldname": "balance",
+            "label": _("Balance"),
+            "fieldtype": "Currency",
+            "width": 130
+        }
+    ]
+
+def get_data(filters):
+    data = []
+    
+    # Get opening balance
+    opening_balance = get_opening_balance(filters)
+    balance = opening_balance
+    
+    # Add opening balance row
+    data.append({
+        "posting_date": formatdate(filters.get("from_date"), "dd-MMM-yy"),
+        "voucher_no": "",  # Empty for opening balance
+        "remarks": "Opening Balance",
+        "debit": 0,
+        "credit": 0,
+        "balance": opening_balance
+    })
+    
+    conditions = get_conditions(filters)
+    
+    gl_entries = frappe.db.sql("""
+        SELECT 
+            posting_date,
+            voucher_type,
+            voucher_no,
+            account,
+            against,
+            debit,
+            credit,
+            remarks
+        FROM `tabGL Entry`
+        WHERE account in (
+            SELECT name 
+            FROM tabAccount 
+            WHERE account_type in ('Bank', 'Cash')
+        )
+        AND {conditions}
+        ORDER BY posting_date, creation
+    """.format(conditions=conditions), filters, as_dict=1)
+
+    for entry in gl_entries:
+        balance += (entry.debit - entry.credit)
+        row = {
+            "posting_date": formatdate(entry.posting_date, "dd-MMM-yy"),
+            "voucher_type": entry.voucher_type,  # Added voucher_type
+            "voucher_no": entry.voucher_no,
+            "remarks": entry.remarks,
+            "debit": entry.debit,
+            "credit": entry.credit,
+            "balance": balance
+        }
+        data.append(row)
+    
+    # Add closing balance row
+    data.append({
+        "posting_date": formatdate(filters.get("to_date"), "dd-MMM-yy"),
+        "voucher_no": "",  # Empty for closing balance
+        "remarks": "Closing Balance",
+        "debit": 0,
+        "credit": 0,
+        "balance": balance
+    })
+    
+    return data
+
+def get_opening_balance(filters):
+    # Get balance before from_date
+    balance = frappe.db.sql("""
+        SELECT SUM(debit) - SUM(credit) as balance
+        FROM `tabGL Entry`
+        WHERE account in (
+            SELECT name 
+            FROM tabAccount 
+            WHERE account_type in ('Bank', 'Cash')
+        )
+        AND posting_date < %(from_date)s
+        AND company=%(company)s
+        {account_condition}
+    """.format(
+        account_condition="AND account=%(account)s" if filters.get("account") else ""
+    ), filters, as_dict=1)[0].balance
+    
+    return balance or 0
+
+def get_conditions(filters):
+    conditions = []
+    
+    if filters.get("company"):
+        conditions.append("company=%(company)s")
+    if filters.get("from_date"):
+        conditions.append("posting_date>=%(from_date)s")
+    if filters.get("to_date"):
+        conditions.append("posting_date<=%(to_date)s")
+    if filters.get("account"):
+        conditions.append("account=%(account)s")
+        
+    return " AND ".join(conditions)
