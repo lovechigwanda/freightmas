013da413cef31a7166f8b98c2a0a1e5b3b55998c
Lovemore Chigwanda <lchigwanda@outlook.com>
2025-12-10 10:31:38 +0200

    Added Bunding Charges in Sales Invoice

--- freightmas/hooks.py
+++ freightmas/hooks.py
@@ -314,6 +314,7 @@ fixtures = [
                 "Purchase Invoice-custom_is_forwarding_invoice",
                 "Sales Invoice-custom_road_freight_job_reference",
                 "Sales Invoice-custom_is_road_freight_invoice",
+                "Sales Invoice-custom_bundle_charges",
                 "Purchase Invoice-custom_road_freight_job_reference",
                 "Purchase Invoice-custom_is_road_freight_invoice",
                 "Sales Invoice-custom_banking_details",
