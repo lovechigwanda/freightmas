7bfde2d4837b4b4f9e7297e8eef02a441ac4cc4a
Lovemore Simbarashe Chigwanda <111510895+lovechigwanda@users.noreply.github.com>
2026-01-06 23:42:04 +0200

    Update fm_accounts_receivable_outstanding.js
    updated code

--- freightmas/freightmas/report/fm_accounts_receivable_outstanding/fm_accounts_receivable_outstanding.js
+++ freightmas/freightmas/report/fm_accounts_receivable_outstanding/fm_accounts_receivable_outstanding.js
@@ -1,71 +1,64 @@
-// Copyright (c) 2026, Zvomaita Technologies (Pvt) Ltd and contributors  
+// Copyright (c) 2026, Zvomaita Technologies (Pvt) Ltd
 // For license information, please see license.txt
 
 frappe.query_reports["FM Accounts Receivable Outstanding"] = {
-    "filters": [
+    filters: [
         {
-            "fieldname": "company",
-            "label": __("Company"),
-            "fieldtype": "Link",
-            "options": "Company",
-            "default": frappe.defaults.get_user_default("Company"),
-            "reqd": 1
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            default: frappe.defaults.get_user_default("Company"),
+            reqd: 1
         },
         {
-            "fieldname": "report_date",
-            "label": __("Report Date"),
-            "fieldtype": "Date",
-            "default": frappe.datetime.get_today(),
-            "reqd": 1
+            fieldname: "customer",
+            label: __("Customer"),
+            fieldtype: "Link",
+            options: "Customer"
         },
         {
-            "fieldname": "customer",
-            "label": __("Customer"),
-            "fieldtype": "Link",
-            "options": "Customer"
-        },
-        {
-            "fieldname": "customer_group",
-            "label": __("Customer Group"),
-            "fieldtype": "Link", 
-            "options": "Customer Group"
-        },
-        {
-            "fieldname": "include_proforma_invoices",
-            "label": __("Include Proforma Invoices"),
-            "fieldtype": "Check",
-            "default": 0
+            fieldname: "include_draft_invoices",
+            label: __("Include Draft Invoices"),
+            fieldtype: "Check",
+            default: 0
         }
     ],
 
-    "formatter": function (value, row, column, data, default_formatter) {
+    formatter: function (value, row, column, data, default_formatter) {
         value = default_formatter(value, row, column, data);
-        
-        if (column.fieldname == "outstanding_amount" && data && data.outstanding_amount > 0) {
-            value = `<span style='color:red!important; font-weight:bold'>${value}</span>`;
+
+        if (!data) return value;
+
+        // Highlight total outstanding
+        if (column.fieldname === "total_outstanding" && data.total_outstanding > 0) {
+            value = `<span style="color:#c0392b; font-weight:600">${value}</span>`;
         }
-        
+
         return value;
     },
 
-    "onload": function(report) {
-        // Add Export buttons
-        report.page.add_inner_button('Export to Excel', function() {
+    onload: function (report) {
+
+        report.page.add_inner_button(__('Export to Excel'), function () {
             const filters = report.get_filter_values(true);
             const query = encodeURIComponent(JSON.stringify(filters));
-            const url = `/api/method/freightmas.api.export_report_to_excel?report_name=FM Accounts Receivable Outstanding&filters=${query}`;
+            const url = `/api/method/freightmas.api.export_report_to_excel`
+                + `?report_name=FM Accounts Receivable Outstanding`
+                + `&filters=${query}`;
             window.open(url);
-        }, 'Export');
+        }, __('Export'));
 
-        report.page.add_inner_button('Export to PDF', function() {
+        report.page.add_inner_button(__('Export to PDF'), function () {
             const filters = report.get_filter_values(true);
             const query = encodeURIComponent(JSON.stringify(filters));
-            const url = `/api/method/freightmas.api.export_report_to_pdf?report_name=FM Accounts Receivable Outstanding&filters=${query}`;
+            const url = `/api/method/freightmas.api.export_report_to_pdf`
+                + `?report_name=FM Accounts Receivable Outstanding`
+                + `&filters=${query}`;
             window.open(url);
-        }, 'Export');
+        }, __('Export'));
 
-        // Add Clear Filters button
-        report.page.add_inner_button('Clear Filters', function() {
+        report.page.add_inner_button(__('Clear Filters'), function () {
             report.filters.forEach(filter => {
                 let default_value = filter.df.default || "";
                 if (filter.df.fieldtype === "Check") {
@@ -75,12 +68,11 @@ frappe.query_reports["FM Accounts Receivable Outstanding"] = {
             });
             report.refresh();
         });
-        
-        // Add refresh button
-        report.page.add_inner_button('Refresh Data', function() {
+
+        report.page.add_inner_button(__('Refresh Data'), function () {
             report.refresh();
         });
     },
 
-    "initial_depth": 0
+    initial_depth: 0
 };
